908177bbf9e23d75a5c08f8ec8b66d7f981aa137
superclaw <makskirclaw@gmail.com>
2020-05-10 15:26:54 +0700

    Replacing uglify with uglify-es, re-compression scripts.js, media__button interactivity fix

--- Landing/gulpfile.js
+++ Landing/gulpfile.js
@@ -1,11 +1,10 @@
 var gulp = require('gulp'),
-	rename = require('gulp-rename'),
-	uglify = require('gulp-uglify'),
-	postcss = require('gulp-postcss'),
-	mqpacker = require('css-mqpacker'),
-	autoprefixer = require('autoprefixer'),
-	csso = require('gulp-csso'),
-  babel = require('gulp-babel');
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify-es').default,
+    postcss = require('gulp-postcss'),
+    mqpacker = require('css-mqpacker'),
+    autoprefixer = require('autoprefixer'),
+    csso = require('gulp-csso');
 
 gulp.task('css', function() {
   return gulp.src('src/css/styles.css')
@@ -23,14 +22,8 @@ gulp.task('cssmin', function() {
     .pipe(gulp.dest('build/css'));
 });
 
-gulp.task('jsconvert', () =>
-  gulp.src('src/js/scripts.js')
-    .pipe(babel({presets: ['env']}))
-    .pipe(gulp.dest('src/js/pre'))
-);
-
 gulp.task('js', function(){
-  return gulp.src('src/js/pre/scripts.js')
+  return gulp.src('src/js/scripts.js')
     .pipe(uglify())
     .pipe(rename('scripts.min.js'))
     .pipe(gulp.dest('build/js'));
